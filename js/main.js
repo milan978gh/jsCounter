3bc585ccfc1474e60e7cbae336343d3eafb4fb64
milan978gh <amilandev78@gmail.com>
2020-11-21 19:03:28 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,28 +1,24 @@
-// set initial count
 let count = 0;
-// select value and buttons
-const value = document.querySelector('#value');
+const value = document.getElementById('value');
 const btns = document.querySelectorAll('.btn');
 
 btns.forEach(function (btn) {
-    btn.addEventListener('click', function (e) {
-        const styles = e.currentTarget.classList;
-        if (styles.contains('decrease')) {
-            count--;
-        } else if (styles.contains('increase')) {
-            count++;
-        } else {
-            count = 0;
-        }
-        if(count>0){
-            value.style.color='green';
-        }
-        if(count<0){
-            value.style.color='red';
-        }
-        if(count===0){
-            value.style.color='#222';
-        }
-        value.textContent = count;
-    });
+  btn.addEventListener('click', function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains(`decrease`)) {
+      count--;
+    } else if (styles.contains('increase')) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count < 0) {
+      value.style.color = 'red';
+    } else if (count > 0) {
+      value.style.color = 'green';
+    } else {
+      value.style.color = '#222';
+    }
+    value.textContent = count;
+  });
 });
