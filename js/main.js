46fc7b6a8cfe763f76b21e7cbc5f7ceb2ac5a78f
milan978gh <amilandev78@gmail.com>
2020-12-29 18:39:22 +0100

    js update

--- js/main.js
+++ js/main.js
@@ -1,24 +1,31 @@
 let count = 0;
 const value = document.getElementById('value');
-const btns = document.querySelectorAll('.btn');
+const decrease = document.querySelector('.decrease');
+const reset = document.querySelector('.reset');
+const increase = document.querySelector('.increase');
 
-btns.forEach(function (btn) {
-  btn.addEventListener('click', function (e) {
-    const styles = e.currentTarget.classList;
-    if (styles.contains(`decrease`)) {
-      count--;
-    } else if (styles.contains('increase')) {
-      count++;
-    } else {
-      count = 0;
-    }
-    if (count < 0) {
-      value.style.color = 'red';
-    } else if (count > 0) {
-      value.style.color = 'green';
-    } else {
-      value.style.color = '#222';
-    }
-    value.textContent = count;
-  });
+
+decrease.addEventListener("click", () => {
+  count--;
+  valueColorNText();
 });
+reset.addEventListener("click", () => {
+  count = 0;
+  valueColorNText();
+});
+increase.addEventListener("click", () => {
+  count++;
+  valueColorNText();
+});
+
+
+function valueColorNText() {
+  value.textContent = count;
+  if (count < 0) {
+    value.style.color = 'red';
+  } else if (count > 0) {
+    value.style.color = 'green';
+  } else {
+    value.style.color = '#102a42';
+  }
+}
